ba865153fa730e2ca3cbdf8f7ab909e2c6639818
vaishnavi185 <vishu032004@gmail.com>
2024-05-21 20:18:45 +0530

    new site culture

---
+++ src/Component/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom'; // Correct import for Link component
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './nav.css';
+
+export default function NavBar() {
+  return (
+    <Navbar bg="transparent" expand="lg">
+      <div className="container-fluid">
+        <Navbar.Brand href="#">
+          <img src='./mount.png' className='im-1' alt='Mount' />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* No changes needed here */}
+        </Navbar.Collapse>
+        <Nav className="ml-auto"> {/* Moved Nav outside Collapse */}
+          <Nav.Link as={Link} to='/'>Home</Nav.Link> {/* Using Link component */}
+          <NavDropdown title="Gallery" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#">Nainital Lake</NavDropdown.Item>
+            <NavDropdown.Item href="#">Mukteshwar Temple</NavDropdown.Item>
+            <NavDropdown.Item href="#">High Altitude Zoo</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#">More</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link as={Link} to='/religion'>Religion</Nav.Link> {/* Using Link component */}
+          <Nav.Link as={Link} to='/about-us'>About us</Nav.Link> {/* Using Link component */}
+        </Nav>
+      </div>
+    </Navbar>
+  );
+}
